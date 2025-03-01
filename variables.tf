85a0820273157c52a04a03b46fb25a6d44b21693
VedantTK <110671057+VedantTK@users.noreply.github.com>
2025-03-01 14:37:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID for authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key for authentication"
+  type        = string
+  sensitive   = true
+}
+
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
